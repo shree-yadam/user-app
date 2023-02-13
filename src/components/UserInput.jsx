3eafbf400ab16d25128d32829245540cc6f3cb40
Shree Devi Yadam <shree.yadam@gmail.com>
2023-02-12 19:54:15 -0800

    Implement user input form state

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,18 +1,47 @@
-import './UserInput.css';
+import { useState } from "react";
+import "./UserInput.css";
 
 function UserInput(props) {
+  const [user, setUser] = useState({ name: "", age: "" });
+
+  /**
+   * Form submission event handler
+   * @param {*} event
+   */
   function submitHandler(event) {
     event.preventDefault();
-    console.log("Submitted");
+    console.log("Submitted ", user);
+  }
+
+  /**
+   * Name change event handler
+   * @param {*} event
+   */
+  function nameChangeHandler(event) {
+    setUser((prev) => {
+      const newUser = { ...prev, name: event.target.value };
+      return newUser;
+    });
+  }
+
+  /**
+   * Age change event handler
+   * @param {*} event
+   */
+  function ageChangeHandler(event) {
+    setUser((prev) => {
+      const newUser = { ...prev, age: event.target.value };
+      return newUser;
+    });
   }
 
   return (
     <form className="user-input" onSubmit={submitHandler}>
       <label htmlFor="username">Username</label>
-      <input id="username"/>
+      <input id="username" value={user.name} onChange={nameChangeHandler} />
       <label htmlFor="age">Age (Years)</label>
-      <input id="age" />
-      <button type='submit'>Add User</button>
+      <input id="age" value={user.age} onChange={ageChangeHandler} />
+      <button type="submit">Add User</button>
     </form>
   );
 }
