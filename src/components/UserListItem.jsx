c131584466b215d6cfccb4ce41e84529c4ae7571
Shree Devi Yadam <shree.yadam@gmail.com>
2023-02-12 20:17:45 -0800

    Format age display

--- src/components/UserListItem.jsx
+++ src/components/UserListItem.jsx
@@ -1,8 +1,13 @@
 import './UserListItem.css';
 
 function UserListItem(props) {
+
+  function formatAge() {
+    return `${props.user.age} year${props.user.age != 1?'s':''} old`;
+  }
+
   return (
-    <div className='user-list-item'> {props.user.name} ({props.user.age} years old)</div>
+    <div className='user-list-item'> {props.user.name} ({formatAge()})</div>
   )
 
 }
