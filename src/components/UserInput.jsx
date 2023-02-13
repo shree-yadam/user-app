ff253c7244f4f14c5bc5bec5169ad29a35838a8f
Shree Devi Yadam <shree.yadam@gmail.com>
2023-02-12 19:46:48 -0800

    Add basic user input form and style

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,8 +1,19 @@
+import './UserInput.css';
+
 function UserInput(props) {
+  function submitHandler(event) {
+    event.preventDefault();
+    console.log("Submitted");
+  }
+
   return (
-    <>
-      Display this!
-    </>
+    <form className="user-input" onSubmit={submitHandler}>
+      <label htmlFor="username">Username</label>
+      <input id="username"/>
+      <label htmlFor="age">Age (Years)</label>
+      <input id="age" />
+      <button type='submit'>Add User</button>
+    </form>
   );
 }
 
