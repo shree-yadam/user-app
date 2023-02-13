6726325e2ea82c01edd8cb7fb519b5b2c1b17195
Shree Devi Yadam <shree.yadam@gmail.com>
2023-02-12 20:30:53 -0800

    Maintain users state in App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 import UserInput from './components/UserInput';
 import UserList from './components/UserList';
 
 function App() {
+  const [users, setUsers] = useState([{name: "Max", age: 31, id: 1}]);
   return (
     <div className="App">
       <UserInput />
-      <UserList users={[{name: "Max", age: 31, id: 1}]}/>
+      <UserList users={users}/>
     </div>
   );
 }
